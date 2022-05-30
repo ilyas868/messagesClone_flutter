2ea2e04bf4a9efebf5d899920b078fee41085150
ilyas868 <ilyas.rahim93@gmail.com>
2022-05-31 00:06:01 +0500

    changes done

--- app/lib/dashboard/sheet.dart
+++ app/lib/dashboard/sheet.dart
@@ -10,23 +10,54 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("flutter App"),
       ),
-      body: Column(
-          children: [
-            listTile(Colors.black38, "ilyas", "kesa h", "12:00 pm"),
-            listTile(Colors.green, "dawood", "hello", "2:00 pm"),
-            listTile(Colors.pink, "sana", "kesa h", "2:20 pm"),
-            listTile(Colors.purple, "salman", "ok", "12:00 pm"),
-            listTile(Colors.black38, "azim", "kesa h", "12:00 pm"),
-            listTile(Colors.brown, "anis", "dfdfsd", "12:00 pm"),
-            listTile(Colors.white, "zubair", "kesa h", "12:00 pm"),
-            listTile(Colors.blue, "ilyas", "kesa h", "12:00 pm"),
-            listTile(Colors.black38, "hamza", "kesa h", "12:00 pm"),
+      body: 
+         Column(
+           children: [
+             Container(
+                height: 150,
+                width: MediaQuery.of(context).size.width *1.0,
+                color: Colors.red,
+                child: Center(
+                  child: Text("slider",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
 
-          ],
-      ) 
+                  ),
+                  
+                  ),
+                ),
+
+             ),
+
+             Expanded(
+               child: ListView(
+                  children: [
+                    listTile(Colors.black38, "shahbaz", "kesa h", "12:00 pm"),
+                    listTile(Colors.green, "dawood", "hello", "2:00 pm"),
+                    listTile(Colors.pink, "sana", "kesa h", "2:20 pm"),
+                    listTile(Colors.purple, "salman", "ok", "12:00 pm"),
+                    listTile(Colors.black38, "azim", "kesa h", "12:00 pm"),
+                    listTile(Colors.brown, "anis", "dfdfsd", "12:00 pm"),
+                    listTile(Colors.white, "zubair", "kesa h", "12:00 pm"),
+                    listTile(Colors.blue, "ilyas", "kesa h", "12:00 pm"),
+                    listTile(Colors.black38, "hamza", "kesa hddfds", "12:00 pm"),
+                    listTile(Colors.purple, "hamza", "kesadsd h", "12:00 pm"),
+                    listTile(Colors.pink, "hamza", "kesadd dsh", "12:00 pm"),
+                    listTile(Colors.blue, "hamza", "kesasdsd h", "12:00 pm"),
+                    listTile(Colors.red, "hamza", "kesa dfdsfh", "12:00 pm"),
+                    listTile(Colors.black, "hamza", "kesdfsda h", "12:00 pm"),
+                   
+                  ],
+                     ),
+             ),
+           ],
+         ),
+      ); 
        
     
       
-    );
+ 
   }
 }
